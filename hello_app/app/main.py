e47461880548c9883bb954af10afe4280deced94
willpeterson <will.peterson.smartsoft@gmail.com>
2023-11-05 21:16:03 -0700

    Hello App

---
+++ hello_app/app/main.py
@@ -0,0 +1,12 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def msg():
+    return "Hellow World! Deployed using Argo CD!"
+
+if __name__ "__main__":
+    app.run(host='0.0.0.0')
+
